adc4a5fd1f91c6e29ea4e609acd6fccfd2885e55
Jamie <jamieianwest@hotmail.com>
2020-02-29 16:02:34 +0000

    Updating vpc/subnet to match the new refactored RiffDog core. RiffDog has been through some major refactoring which is non compat with the way these modules were originally written. Updating them to match the interface etc now.

--- riffdog_aws/resources/vpc/subnet.py
+++ riffdog_aws/resources/vpc/subnet.py
@@ -3,40 +3,35 @@ This module is for the Subnet Reosurce
 """
 import logging
 
-from riffdog.data_structures import ReportElement
-from riffdog.resource import AWSResource, register
+from riffdog.data_structures import FoundItem
+from riffdog.resource import register, ResourceDirectory
+
+from ...aws_resource import AWSResource
 
 logger = logging.getLogger(__name__)
 
 
 @register("aws_subnet")
 class AWSSubnet(AWSResource):
-    _subnets_in_aws = {}
-    _subnets_in_state = {}
+    resource_type = "aws_subnet"
 
     def fetch_real_regional_resources(self, region):
-        logging.info("Looking for aws_subnet resources")
-
+        logging.info("Looking for %s resources..." % self.resource_type)
         ec2 = self._get_resource('ec2', region)
+        rd = ResourceDirectory()
 
         for subnet in ec2.subnets.filter():
-            self._subnets_in_aws[subnet.id] = subnet
+            try:
+                item = rd.get_item(predicted_id=subnet.id)
+                item.real_id = subnet.id
+                item.real_data = subnet
+            except KeyError:
+                FoundItem(self.resource_type, real_id=subnet.id, real_data=subnet)
 
     def process_state_resource(self, state_resource, state_filename):
+        logger.info("Found a resource of type %s!" % self.resource_type)
         for instance in state_resource["instances"]:
-            self._subnets_in_state[instance["attributes"]["id"]] = "instance"
+            FoundItem(self.resource_type, terraform_id=instance["attributes"]["id"], predicted_id=instance["attributes"]["id"], state_data=instance)
 
-    def compare(self, depth):
-        out_report = ReportElement()
-
-        for key, val in self._subnets_in_state.items():
-            if key not in self._subnets_in_aws:
-                out_report.in_tf_but_not_real.append(key)
-            else:
-                out_report.matched.append(key)
-
-        for key, val in self._subnets_in_aws.items():
-            if key not in self._subnets_in_state:
-                out_report.in_real_but_not_tf.append(key)
-
-        return out_report
+    def compare(self, item, depth):
+        pass
--- riffdog_aws/resources/vpc/vpc.py
+++ riffdog_aws/resources/vpc/vpc.py
@@ -3,40 +3,35 @@ This module is for the VPC Reosurce
 """
 import logging
 
-from riffdog.data_structures import ReportElement
-from riffdog.resource import AWSResource, register
+from riffdog.data_structures import FoundItem
+from riffdog.resource import register, ResourceDirectory
+
+from ...aws_resource import AWSResource
 
 logger = logging.getLogger(__name__)
 
 
 @register("aws_vpc")
 class AWSVPC(AWSResource):
-    _vpcs_in_aws = {}
-    _vpcs_in_state = {}
+    resource_type = "aws_vpc"
 
     def fetch_real_regional_resources(self, region):
-        logging.info("Looking for aws_vpc resources")
-
+        logging.info("Looking for %s resources..." % self.resource_type)
         ec2 = self._get_resource('ec2', region)
+        rd = ResourceDirectory()
 
         for vpc in ec2.vpcs.filter():
-            self._vpcs_in_aws[vpc.id] = vpc
+            try:
+                item = rd.get_item(predicted_id=vpc.id)
+                item.real_id = vpc.id
+                item.real_data = vpc
+            except KeyError:
+                FoundItem(self.resource_type, real_id=vpc.id, real_data=vpc)
 
     def process_state_resource(self, state_resource, state_filename):
+        logger.info("Found a resource of type %s!" % self.resource_type)
         for instance in state_resource["instances"]:
-            self._vpcs_in_state[instance["attributes"]["id"]] = instance
+            FoundItem(self.resource_type, terraform_id=instance["attributes"]["id"], predicted_id=instance["attributes"]["id"], state_data=instance)
 
-    def compare(self, depth):
-        out_report = ReportElement()
-
-        for key, val in self._vpcs_in_state.items():
-            if key not in self._vpcs_in_aws:
-                out_report.in_tf_but_not_real.append(key)
-            else:
-                out_report.matched.append(key)
-
-        for key, val in self._vpcs_in_aws.items():
-            if key not in self._vpcs_in_state:
-                out_report.in_real_but_not_tf.append(key)
-
-        return out_report
+    def compare(self, item, depth):
+        pass
